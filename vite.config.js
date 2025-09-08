d1ebacaebf13a89904d728d9d5998f92c2f89e9e
nana-kwao <nanakwaofreiku@outlook.com>
2025-09-08 19:53:25 +0000

    minor update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
   server: {
-    proxy :{
+    proxy: {
       "/api": {
         target: "https://inventory-server-tmqz.onrender.com:10000",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
-build: {
-    outDir: 'dist',
-    // Generate index.html for all routes
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
-})
+  build: {
+    outDir: "dist",
+  },
+});
