9c7c00f1e7aad656fff0708fb6253f6f8c6b94d8
nana-kwao <nanakwaofreiku@outlook.com>
2025-09-09 04:10:41 +0000

    minor fix | error message

--- src/assets/services/loginService.js
+++ src/assets/services/loginService.js
@@ -3,10 +3,7 @@ import authAPI from "./authService";
 const loginService = async (userData) => {
   try {
     const { data } = await authAPI.post("/login", userData);
-    if (data.success) {
-      return data;
-    }
-    return JSON.stringify({ error: "Request Failed" });
+    return data;
   } catch (error) {
     console.error("Login failed:", error);
     throw error;
