7fe51289508de45a98bbc2b33b17e718a4fee51c
nana-kwao <nanakwaofreiku@outlook.com>
2025-09-15 23:59:17 +0000

    minor ui update && introduced Products store

--- src/assets/store/MenuSlice.js
+++ src/assets/store/MenuSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  menuItemText: false,
+  menuItemText: true,
 };
 
 const MenuSlice = createSlice({
--- src/assets/store/store.js
+++ src/assets/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from "./UserSlice.js";
 import MenuSlice from "./MenuSlice.js";
+import ProductSlice from "./ProductSlice.js";
 
 const store = configureStore({
   reducer: {
     User: UserReducer,
     Menu: MenuSlice,
+    Products: ProductSlice,
   },
 });
 
